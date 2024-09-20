f72698d22dd65777f48190c6fc6a61dc30484ff9
JYCruz <josh.cruz@neu.edu.ph>
2024-09-20 11:36:40 +0800

    Update AttendanceSystemAdapter.java

--- main/AttendanceSystemAdapter.java
+++ main/AttendanceSystemAdapter.java
@@ -1,12 +1,14 @@
-
-public class AttendanceSystemAdapter implements SchoolManagementApp{
+public class AttendanceSystemAdapter implements SchoolManagementApp {
     private AttendanceSystem attendanceSystem;
 
-    public AttendanceSystemAdapter(AttendanceSystem attendanceSystem){
+    public AttendanceSystemAdapter(AttendanceSystem attendanceSystem) {
         this.attendanceSystem = attendanceSystem;
     }
+
     @Override
-    public void integrateSystem(){
-        attendanceSystem.markAttendance();
+    public void integrateSystem(String... args) {
+        if (args.length > 0) {
+            attendanceSystem.markAttendance(args[0]);
+        }
     }
 }
