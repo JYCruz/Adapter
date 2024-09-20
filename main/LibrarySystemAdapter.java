7e6a554354604408f0de67d964bffc89e3c8bc07
JYCruz <josh.cruz@neu.edu.ph>
2024-09-20 11:35:58 +0800

    Update LibrarySystemAdapter.java

--- main/LibrarySystemAdapter.java
+++ main/LibrarySystemAdapter.java
@@ -1,11 +1,14 @@
-public class LibrarySystemAdapter implements SchoolManagementApp{
+public class LibrarySystemAdapter implements SchoolManagementApp {
     private LibrarySystem librarySystem;
 
-    public LibrarySystemAdapter(LibrarySystem librarySystem){
+    public LibrarySystemAdapter(LibrarySystem librarySystem) {
         this.librarySystem = librarySystem;
     }
+
     @Override
-    public void integrateSystem(){
-        librarySystem.manageBooks();
+    public void integrateSystem(String... args) {
+        if (args.length > 1) {
+            librarySystem.manageBooks(args[0], args[1]);
+        }
     }
 }
