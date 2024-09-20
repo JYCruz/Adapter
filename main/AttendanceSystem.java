c9a2e4be0a2b01992e31cce1be5838c2cb8ce0b9
JYCruz <josh.cruz@neu.edu.ph>
2024-09-20 11:36:26 +0800

    Update AttendanceSystem.java

--- main/AttendanceSystem.java
+++ main/AttendanceSystem.java
@@ -1,5 +1,5 @@
-public class AttendanceSystem{
-    void markAttendance(){
-        System.out.println("Marking attendance!");
+public class AttendanceSystem {
+    public void markAttendance(String studentId) {
+        System.out.println("Attendance marked for student: " + studentId);
     }
 }
