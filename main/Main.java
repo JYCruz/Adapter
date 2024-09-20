571598777e55ab98dee18c54dbe06540a75d3ee9
JYCruz <josh.cruz@neu.edu.ph>
2024-09-20 10:47:05 +0800

    Update Main.java

--- main/Main.java
+++ main/Main.java
@@ -1,1 +1,16 @@
-a
+public class Main{
+    public static void main(String args[]){
+        AttendanceSystem attendanceSystem = new AttendanceSystem();
+        GradingSystem gradingSystem = new GradingSystem();
+        LibrarySystem librarySystem = new LibrarySystem();
+
+        AttendanceSystemAdapter attendanceSystemAdapter = new AttendanceSystemAdapter(attendanceSystem);
+        GradingSystemAdapter GradingSystemAdapter = new GradingSystemAdapter(gradingSystem);
+        LibrarySystemAdapter librarySystemAdapter = new LibrarySystemAdapter(librarySystem);
+        
+        attendanceSystemAdapter.integrateSystem();
+        GradingSystemAdapter.integrateSystem();
+        librarySystemAdapter.integrateSystem();
+
+    }
+}
