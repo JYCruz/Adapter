b5997c00f7ee865e0b3885295c903ff90e9df33f
JYCruz <josh.cruz@neu.edu.ph>
2024-09-20 11:42:15 +0800

    Update GradingSystem.java

--- main/GradingSystem.java
+++ main/GradingSystem.java
@@ -1,5 +1,5 @@
 public class GradingSystem {
     public void recordGrades(String studentId, String grade) {
-        System.out.println("Grade recorded for student: " + studentId + ", Grade: " + grade);
+        System.out.println("Grade recorded for student #" + studentId + ", Grade: " + grade);
     }
 }
